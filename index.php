0c7391532c24822eca9d9e527f1bde22a3524c2a
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-06 09:06:41 -0300

    aula

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <?php
+
+        $msg = 'Meu primeiro arquivo em PHP!';
+
+        $fichacompleta = $nome. "-" .$idade. "-" $salario;
+        echo $fichacompleta;
+    ?>
+    
+</body>
+</html>
