57f4670b2c219a51851adc85cb579bc0fc69a8d9
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-06 09:18:13 -0300

    aula

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula PHP</title>
+    <title>Aula de PHP</title>
 </head>
 <body>
     <?php
 
-        $msg = 'Meu primeiro arquivo em PHP!';
+       $msg = "Meu primeiro arquivo em PHP";
+       echo $msg;
 
-        $fichacompleta = $nome. "-" .$idade. "-" $salario;
-        echo $fichacompleta;
+       $nome = 'Lucas';
+       $idade = '20';
+       $salario = '150';
+       $fichacompleta = $nome."-".$idade."-".$salario;
+       echo "Meu nome é {$nome} - minha idade é {$idade} - meu salário é {$salario}";
+
     ?>
-    
 </body>
 </html>
