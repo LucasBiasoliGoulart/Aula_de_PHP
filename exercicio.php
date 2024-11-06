6b28206d6d976b723fad8b16122e006d53a41805
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-06 09:41:02 -0300

    exercicio

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício de PHP</title>
+</head>
+<body>
+    <?php
+        // Exercícios
+        // A
+        $a = 18;
+        $b = -43;
+        $c = 65;
+        $resposta = ($c - $b)/$a;
+        echo "O valor de X é {$resposta} <br>";
+
+        // B
+        $a = 6;
+        $b = 8;
+        $c = 5;
+        $d = 12;
+        $e = 21;
+        $resposta1 = ($a * $b/($c*$d) + $e);
+        echo "O valor é {$resposta1} <br>";
+
+    ?>
+</body>
+</html>
