529a39e0d01de1fa7be40366288251ca7b9595b0
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-08 08:33:38 -0300

    aula

---
+++ atividade2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ano anterior</title>
+</head>
+<body>
+    <?php
+        $ano = 2024;
+        $anterior = 2023;
+        echo "O ano atual é $ano e o ano anterior é ".$anterior;
+    ?>
+</body>
+</html>
---
+++ variaves.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaves</title>
+</head>
+<body>
+    <?php
+        $A = 5;
+        $B = 7;
+        $soma = 5 + 7;
+        echo "A variavel A vale $A <br>";
+        echo "A variavel B vale $B <br>";
+        echo "A soma das variaves A e B é $soma";
+    ?>
+</body>
+</html>
