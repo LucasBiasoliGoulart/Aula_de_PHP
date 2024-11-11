37df75b5b3e7c5bc76fd5723db76e39a03ec30a1
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-11 11:46:12 -0300

    helpdesk

---
+++ HelpDesk/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HelpDesk</title>
+</head>
+<body>
+    <nav>
+        <img src="./Img/logo.png" alt="Logo">
+        <h1>HelpDesk</h1>
+    </nav>
+    <style>
+        body{
+            width: 100%;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
+            font-family: arial;
+        }
+        nav{
+            width: 100%;
+            height: 10vh;
+            display: flex;
+            align-items: center;
+            background: black;
+            color: white;
+        }
+
+        nav img{
+            width: 50px;
+            height: 50px;
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+
+        nav h1{
+            font-weight: 400;
+        }
+    </style>
+</body>
+</html>
