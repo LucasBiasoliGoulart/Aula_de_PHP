d19345c94853286c7d13ac3cfedb76af0b7a2e8a
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-11-12 08:31:44 -0300

    helpdesk

--- HelpDesk/index.php
+++ HelpDesk/index.php
@@ -76,7 +76,10 @@
         input{
             width: 100%;
             height: 50px;
-            margin-bottom: 15px;
+            border: 1px solid gray;
+            border-radius: 10px;
+            margin-bottom: 25px;
+            background-color: #F3F3F3;
         }
 
         button{
