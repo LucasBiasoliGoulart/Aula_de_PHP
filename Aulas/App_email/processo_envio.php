820b8de8fac8204407f85428ee89af2037068de2
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-02-12 11:40:31 -0300

    aula

--- Aulas/App_email/processo_envio.php
+++ Aulas/App_email/processo_envio.php
@@ -9,13 +9,64 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 class Email {
-    private $email = null;
+    private $para = null;
     private $assunto = null;
     private $mensagem = null;
     public $status = array('codigo_status' => null, 'descricao_status' => '');
 
     public function __set($atributos, $valores) {
         $this->$atributos = $valores;
+    }
+
+    public function __get($atributos) {
+        return $this->$atributos;
+    }
+
+    public function mensagemValida() {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+            return false;
+        }
+            return true;
+    }
+}
+
+$mensagem = new Mensagem();
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+if(!$mensagem-> mensagemValida()) {
+    header('Location: index.php');
+}else {
+    $email = new PHPMailer(true);
+
+    try{
+        // Server Setting
+        $email->SMTDebug   = false;
+        $email->isSMTP();
+        $email->Host       = 'smtp.example.com';
+        $email->SMTPAuth   = true;
+        $email->Username   = 'user@example.com';
+        $email->Password   = 'secret';
+        $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $email->Port       = 465;
+
+        // Reciptions
+        $email->setForm('form@example.com', 'App_email');
+        $email->addAdress($mensagem->__get('para'));
+
+        // Content
+        $email->isHTML(true);
+        $email->Subject = $mensagem->__get('assunto')
+        $email->Body    = $mensagem->__get('mensagem')
+        $email->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso total ao conteúdo desta mensagem!';
+
+        $email->send();
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = 'E-mail enivado com sucesso!';
+    }catch (Exception $e) {
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = 'Não foi possivel enviar este e-mail! Contate o Administrador! Detalhes do erro: ' . $email->ErrorInfo;
     }
 }
 ?>
@@ -39,11 +90,20 @@ class Email {
     </div>
     <div style="width: 100%; height: 40vh; display: flex; align-items: center; justify-content: center;">
     <div class="card" style="width: 30%;">
-        <div class="card-body">
-            <h1 class="card-title text-success">Sucesso</h1>
-            <p class="card-text text-success">E-mail enviado com seucesso!</p>
-            <button class="btn btn-success">Voltar</button>
-        </div>
+        <?php if ($mensagem->status['codigo_status'] == 1){ ?>
+            <div class="card-body bg-success-subtle border-success-subtle">
+                <h1 class="card-title text-success">Sucesso</h1>
+                <p><?= $mensagem->status['descricao_status']; ?></p>
+                <a href="./index.php" class="btn btn-success text-white">Voltar</a>
+            </div>
+        <?php } ?>
+        <?php if ($mensagem->status['codigo_status'] == 1){ ?>
+            <div class="card-body bg-danger-subtle border-success-subtle">
+                <h1 class="card-title text-danger">Sucesso</h1>
+                <p><?= $mensagem->status['descricao_status']; ?></p>
+                <a href="./index.php" class="btn btn-danger text-white">Voltar</a>
+            </div>
+        <?php } ?>
     </div>
     </div>
 </body>
