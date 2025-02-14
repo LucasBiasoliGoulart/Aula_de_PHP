8bc7ff82cdc60985a2ce726c16999c3f28dd396f
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2025-02-14 09:45:40 -0300

    aula

--- Aulas/Php_academy/correspondencia.php
+++ Aulas/Php_academy/correspondencia.php
@@ -1,9 +1,13 @@
 <?php
 // Correspondência de Expressão
+$busca = 1;
+
 $retorno = match ($busca) {
     '1' => 'Encontrou o texto 1',
     1 => 'Encontrou o número 1',
     10, 20, 30 => 'Encontrou o número 10, 20 ou 30',
     default => 'Retorno se nenhuma condição for satisfeita'
 };
+
+echo $retorno;
 ?>
